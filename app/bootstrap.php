e56088230c4409901620ed2f93e4b465b12935ae
webman <16526157+webman@users.noreply.github.com>
2025-01-12 04:01:04 +0500

    fix phacility/libphutil package usage

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -5,7 +5,8 @@ error_reporting(E_ALL);
 require_once __DIR__ . '/../vendor/autoload.php';
 
 // libphutil provides static functions that can't be autoloaded by Composer
-require_once __DIR__ . '/../vendor/facebook/libphutil/src/__phutil_library_init__.php';
+// replaced facebook to phacility (thanks to West14)
+require_once __DIR__ . '/../vendor/phacility/libphutil/src/__phutil_library_init__.php';
 
 // If libphutil is the last autoloader (Composer prepends, so will never be after it), it throws on missing classes...
 spl_autoload_register(function ($class) {});
